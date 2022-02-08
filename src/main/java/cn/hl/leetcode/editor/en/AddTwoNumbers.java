67b302dca5d4920503c647bbb52b8b48721f569e
hulin08 <hu382018@outlook.com>
2022-02-08 10:16:58 +0800

    leetcode day1

---
+++ src/main/java/cn/hl/leetcode/editor/en/AddTwoNumbers.java
@@ -0,0 +1,103 @@
+package cn.hl.leetcode.editor.en;
+//You are given two non-empty linked lists representing two non-negative 
+//integers. The digits are stored in reverse order, and each of their nodes contains a 
+//single digit. Add the two numbers and return the sum as a linked list. 
+//
+// You may assume the two numbers do not contain any leading zero, except the 
+//number 0 itself. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: l1 = [2,4,3], l2 = [5,6,4]
+//Output: [7,0,8]
+//Explanation: 342 + 465 = 807.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: l1 = [0], l2 = [0]
+//Output: [0]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//Output: [8,9,9,9,0,0,0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in each linked list is in the range [1, 100]. 
+// 0 <= Node.val <= 9 
+// It is guaranteed that the list represents a number that does not have 
+//leading zeros. 
+// 
+// Related Topics Linked List Math Recursion 👍 16230 👎 3470
+
+public class AddTwoNumbers{
+      public static void main(String[] args) {
+          Solution solution = new AddTwoNumbers().new Solution();
+          ListNode l1 = new ListNode(2);
+          ListNode l2 = new ListNode(4);
+          ListNode l3 = new ListNode(3);
+          ListNode l4 = new ListNode(5);
+          ListNode l5 = new ListNode(6);
+          ListNode l6 = new ListNode(4);
+          l1.next = l2;
+          l2.next = l3;
+          l4.next = l5;
+          l5.next = l6;
+          ListNode result = solution.addTwoNumbers(l1, l4);
+          while (result != null){
+              System.out.println(result.val);
+              result = result.next;
+          }
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        boolean plus = false;
+        while(l1 != null || l2 != null){
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+            int v3 = v1 + v2;
+            if(plus) v3++;
+            plus = v3 >= 10 ? true : false;
+            current.next = new ListNode(v3 % 10);
+            current = current.next;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        if(plus){
+            current.next = new ListNode(1);
+        }
+        return head.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+  }
---
+++ src/main/java/cn/hl/leetcode/editor/en/TwoSum.java
@@ -0,0 +1,92 @@
+package cn.hl.leetcode.editor.en;
+//Given an array of integers nums and an integer target, return indices of the 
+//two numbers such that they add up to target. 
+//
+// You may assume that each input would have exactly one solution, and you may 
+//not use the same element twice. 
+//
+// You can return the answer in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// Only one valid answer exists. 
+// 
+//
+// 
+//Follow-up: Can you come up with an algorithm that is less than O(n²) time 
+//complexity? Related Topics Array Hash Table 👍 28933 👎 924
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum{
+      public static void main(String[] args) {
+          Solution solution = new TwoSum().new Solution();
+          int [] nums = new int[]{3,2,4};
+          int[] result = solution.twoSum2(nums, 6);
+          for (int r : result){
+              System.out.println(r);
+          }
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum1(int[] nums, int target) {
+        if(nums == null || nums.length < 2){
+            return new int[]{};
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length ; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+        public int[] twoSum2(int[] nums, int target) {
+            if(nums == null || nums.length < 2){
+                return null;
+            }
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int complement = target - nums[i];
+                if(map.containsKey(complement)){
+                    return new int[]{map.get(complement), i};
+                }
+                map.put(nums[i], i);
+            }
+            return null;
+        }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
