b16adb84899852c6592591cd48eddaae62f39d14
HULIN <hu382018@outlook.com>
2022-02-11 09:57:06 +0800

    lc-21

---
+++ src/main/java/cn/hl/leetcode/editor/en/MergeKSortedLists.java
@@ -0,0 +1,78 @@
+package cn.hl.leetcode.editor.en;
+//You are given an array of k linked-lists lists, each linked-list is sorted in 
+//ascending order. 
+//
+// Merge all the linked-lists into one sorted linked-list and return it. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: lists = [[1,4,5],[1,3,4],[2,6]]
+//Output: [1,1,2,3,4,4,5,6]
+//Explanation: The linked-lists are:
+//[
+//  1->4->5,
+//  1->3->4,
+//  2->6
+//]
+//merging them into one sorted list:
+//1->1->2->3->4->4->5->6
+// 
+//
+// Example 2: 
+//
+// 
+//Input: lists = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: lists = [[]]
+//Output: []
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// k == lists.length 
+// 0 <= k <= 10⁴ 
+// 0 <= lists[i].length <= 500 
+// -10⁴ <= lists[i][j] <= 10⁴ 
+// lists[i] is sorted in ascending order. 
+// The sum of lists[i].length will not exceed 10⁴. 
+// 
+// Related Topics Linked List Divide and Conquer Heap (Priority Queue) Merge 
+//Sort 👍 11066 👎 443
+
+import java.util.PriorityQueue;
+
+public class MergeKSortedLists{
+    public static void main(String[] args) {
+        Solution solution = new MergeKSortedLists().new Solution();
+        
+        System.out.println();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq= new PriorityQueue<>();
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
